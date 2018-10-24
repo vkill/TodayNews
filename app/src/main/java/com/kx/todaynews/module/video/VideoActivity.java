6a85bb440c7405a9d279c1d58679905447e405a6
kk9923 <13047381556@163.com>
2018-10-24 18:01:39 +0800

    更新视频播放

--- app/src/main/java/com/kx/todaynews/module/video/VideoActivity.java
+++ app/src/main/java/com/kx/todaynews/module/video/VideoActivity.java
@@ -1,18 +1,18 @@
 package com.kx.todaynews.module.video;
 
-import android.hardware.Sensor;
-import android.hardware.SensorManager;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Base64;
+import android.view.KeyEvent;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.dl7.player.media.IjkPlayerView;
 import com.kx.todaynews.Api;
 import com.kx.todaynews.R;
 import com.kx.todaynews.net.NetClient;
 import com.kx.todaynews.utils.LogUtils;
-import com.kx.todaynews.widget.helper.MyJZVideoPlayerStandard;
 
 import java.util.Random;
 import java.util.zip.CRC32;
@@ -34,6 +34,8 @@ public class VideoActivity extends AppCompatActivity {
     TextView tvUrl;
     @BindView(R.id.videoplayer)
     JZVideoPlayerStandard videoPlayer;
+    @BindView(R.id.video_player)
+    IjkPlayerView ijkPlayerView;
     private String mItemId;
     private String mVideoId;
 
@@ -58,6 +60,9 @@ public class VideoActivity extends AppCompatActivity {
                             String base64 = videoList.getVideo_1().getMain_url();
                             String url1 = (new String(Base64.decode(base64.getBytes(), Base64.DEFAULT)));
                             videoPlayer.setUp(url1, JZVideoPlayerStandard.SCREEN_WINDOW_NORMAL, getIntent().getStringExtra(TITLETEXTVIEW));
+                            ijkPlayerView.init()
+                                    .setTitle( getIntent().getStringExtra(TITLETEXTVIEW))
+                                    .setVideoSource(null, url1, null, null, null);
                             stringBuilder.append("url1=" + url1 + "\n");
                         }
                         if (videoList.getVideo_2() != null) {
@@ -106,16 +111,48 @@ public class VideoActivity extends AppCompatActivity {
         }
         return result.toString();
     }
+
     @Override
     public void onBackPressed() {
         if (JZVideoPlayer.backPress()) {
             return;
         }
+        if (ijkPlayerView.onBackPressed()) {
+            return;
+        }
         super.onBackPressed();
     }
+
     @Override
     protected void onPause() { //选择适当的声明周期释放
         super.onPause();
         JZVideoPlayer.releaseAllVideos();
+        ijkPlayerView.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        ijkPlayerView.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ijkPlayerView.onDestroy();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        ijkPlayerView.configurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (ijkPlayerView.handleVolumeKey(keyCode)) {
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
