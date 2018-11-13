5246f6d087052089d3d43029818bbd57a19a9ece
kk9923 <13047381556@163.com>
2018-11-13 23:23:58 +0800

    update Map module

--- app/src/main/java/com/kx/todaynews/module/map/MapFragment.java
+++ app/src/main/java/com/kx/todaynews/module/map/MapFragment.java
@@ -1,27 +1,43 @@
 package com.kx.todaynews.module.map;
 
+import android.Manifest;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.amap.api.maps2d.AMap;
-import com.amap.api.maps2d.CameraUpdateFactory;
-import com.amap.api.maps2d.MapView;
-import com.amap.api.maps2d.model.MyLocationStyle;
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.CameraUpdateFactory;
+import com.amap.api.maps.MapView;
+import com.amap.api.maps.SupportMapFragment;
+import com.amap.api.maps.model.LatLng;
+import com.amap.api.maps.model.Marker;
+import com.amap.api.maps.model.MarkerOptions;
+import com.amap.api.maps.model.MyLocationStyle;
 import com.kx.todaynews.R;
+import com.kx.todaynews.utils.LogUtils;
+import com.kx.todaynews.utils.ToastUtils;
+import com.tbruyelle.rxpermissions2.RxPermissions;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import butterknife.Unbinder;
 
-public class MapFragment extends Fragment {
+public class MapFragment extends SupportMapFragment implements AMapLocationListener {
 
     @BindView(R.id.map)
     MapView mMapView;
     Unbinder unbinder;
+    private AMap mAMap;
 
     public static MapFragment getInstance() {
         return new MapFragment();
@@ -33,27 +49,120 @@ public class MapFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_map, container, false);
         unbinder = ButterKnife.bind(this, rootView);
         mMapView.onCreate(savedInstanceState);
-        //初始化地图控制器对象
-        AMap aMap = null;
-        if (aMap == null) {
-            aMap = mMapView.getMap();
-        }
-       // aMap.setTrafficEnabled(true);// 显示实时交通状况
-        //MyLocationStyle myLocationStyle = new MyLocationStyle();//初始化定位蓝点样式类myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
-       // myLocationStyle.interval(2000); //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
-       // aMap.setMyLocationStyle(myLocationStyle);//设置定位蓝点的Style
-        //aMap.getUiSettings().setMyLocationButtonEnabled(true);设置默认定位按钮是否显示，非必需设置。
-       // aMap.setMyLocationEnabled(true);// 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
-      //  aMap.moveCamera(CameraUpdateFactory.zoomTo(17));
-       // myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_FOLLOW);//连续定位、且将视角移动到地图中心点，定位蓝点跟随设备移动
         return rootView;
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        RxPermissions rxPermissions = new RxPermissions(this);
+        rxPermissions.requestEachCombined(Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.READ_EXTERNAL_STORAGE)
+                .subscribe(permission -> {
+                    if (permission.granted) {
+                        // All permissions are granted !
+                    } else if (permission.shouldShowRequestPermissionRationale){
+                        // At least one denied permission without ask never again
+                    } else {
+
+                    }
+                });
+    }
+    //声明AMapLocationClient类对象
+    public AMapLocationClient mLocationClient = null;
+    //声明AMapLocationClientOption对象
+    public AMapLocationClientOption mLocationOption = null;
+    private boolean followMove=true;
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        //初始化地图控制器对象
+        mAMap = null;
+        if (mAMap == null) {
+            mAMap = mMapView.getMap();
+        }
+        // 显示实时交通状况
+        mAMap.setTrafficEnabled(true);
+        /**
+         * MyLocationStyle
+         * https://lbs.amap.com/api/android-sdk/guide/create-map/mylocation
+         */
+        //初始化定位蓝点样式类myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);
+        // 连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
+        MyLocationStyle myLocationStyle = new MyLocationStyle();
+
+        myLocationStyle.interval(2000); //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
+        //设置定位蓝点精度圆圈的填充颜色的方法。
+        myLocationStyle.radiusFillColor(android.R.color.white);
+        //设置定位蓝点精度圆圈的边框颜色的方法。
+        myLocationStyle.strokeColor(android.R.color.white);
+        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE_NO_CENTER);
+
+        mAMap.setMyLocationStyle(myLocationStyle);//设置定位蓝点的Style
+        // aMap.getUiSettings().setMyLocationButtonEnabled(true);// 设置默认定位按钮是否显示，非必需设置。
+
+        mAMap.moveCamera(CameraUpdateFactory.zoomTo(17));
+        /**
+         *  解决拖动地图就会自动回到中心点的问题
+         */
+        mAMap.setOnMyLocationChangeListener(new AMap.OnMyLocationChangeListener() {
+            @Override
+            public void onMyLocationChange(Location location) {
+                if (followMove) {
+                    mAMap.animateCamera(CameraUpdateFactory.newLatLng(new LatLng(location.getLatitude(),location.getLongitude())));
+                }
+            }
+        });
+        mAMap.setOnMapTouchListener(new AMap.OnMapTouchListener() {
+            @Override
+            public void onTouch(MotionEvent motionEvent) {
+                if (followMove) {
+                    //用户拖动地图后，不再跟随移动，直到用户点击定位按钮
+                    followMove = false ;
+                }
+            }
+        });
+
+        mAMap.setMyLocationEnabled(true);
+        //初始化AMapLocationClientOption对象
+        mLocationOption = new AMapLocationClientOption();
+        //初始化定位
+        mLocationClient = new AMapLocationClient(getContext());
+        //设置定位回调监听
+        mLocationClient.setLocationListener(this);
+        //设置定位模式为AMapLocationMode.Hight_Accuracy，高精度模式。
+        mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+        //设置定位间隔,单位毫秒,默认为2000ms，最低1000ms。
+       // mLocationOption.setInterval(3000);
+        //获取一次定位结果：
+        //该方法默认为false。
+        mLocationOption.setOnceLocation(true);
+        //设置是否返回地址信息（默认返回地址信息）
+        mLocationOption.setNeedAddress(true);
+        /**
+         * 设置定位场景，目前支持三种场景（签到、出行、运动，默认无场景）
+         */
+       // mLocationOption.setLocationPurpose(AMapLocationClientOption.AMapLocationPurpose.SignIn);
+        //给定位客户端对象设置定位参数
+        mLocationClient.setLocationOption(mLocationOption);
+        //启动定位
+        mLocationClient.startLocation();
+        /**
+         * 用于重新设置场景模式，
+         * 设置场景模式后最好调用一次stop，再调用start以保证场景模式生效
+         */
+//        if(null != mLocationClient){
+//            mLocationClient.setLocationOption(mLocationOption);
+//            mLocationClient.stopLocation();
+//            mLocationClient.startLocation();
+//        }
+    }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
         //在activity执行onDestroy时执行mMapView.onDestroy()，销毁地图
+        if (mMapView!=null)
         mMapView.onDestroy();
     }
 
@@ -61,6 +170,7 @@ public class MapFragment extends Fragment {
     public void onResume() {
         super.onResume();
         //在activity执行onResume时执行mMapView.onResume ()，重新绘制加载地图
+        if (mMapView!=null)
         mMapView.onResume();
     }
 
@@ -68,6 +178,7 @@ public class MapFragment extends Fragment {
     public void onPause() {
         super.onPause();
         //在activity执行onPause时执行mMapView.onPause ()，暂停地图的绘制
+        if (mMapView!=null)
         mMapView.onPause();
     }
 
@@ -75,6 +186,7 @@ public class MapFragment extends Fragment {
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         //在activity执行onSaveInstanceState时执行mMapView.onSaveInstanceState (outState)，保存地图当前的状态
+        if (mMapView!=null)
         mMapView.onSaveInstanceState(outState);
     }
 
@@ -83,4 +195,31 @@ public class MapFragment extends Fragment {
         super.onDestroyView();
         unbinder.unbind();
     }
+
+    /**
+     * AMapLocation核心方法解析
+     * @param aMapLocation @link{https://lbs.amap.com/api/android-location-sdk/guide/android-location/getlocation}
+     */
+    @Override
+    public void onLocationChanged(AMapLocation aMapLocation) {
+        if (aMapLocation != null) {
+            if (aMapLocation.getErrorCode() == 0) {
+                //可在其中解析amapLocation获取相应内容。
+                LogUtils.e("地址：" +aMapLocation.getAddress() +"   经度："+ aMapLocation.getLongitude() +"  纬度："+aMapLocation.getLatitude());
+                LatLng latLng = new LatLng(aMapLocation.getLatitude(),aMapLocation.getLongitude());
+                final Marker marker = mAMap.addMarker(new MarkerOptions().position(latLng).title("家").snippet("DefaultMarker").draggable(true));
+            }else {
+                //定位失败时，可通过ErrCode（错误码）信息来确定失败的原因，errInfo是错误信息，详见错误码表。
+                ToastUtils.showToast("ErrCode:"
+                        + aMapLocation.getErrorCode() + ", errInfo:"
+                        + aMapLocation.getErrorInfo());
+            }
+        }
+    }
+    @OnClick(R.id.bt_location)
+    public void ReLocate(){
+        mAMap.setMyLocationEnabled(true);
+        followMove = true;
+    }
+
 }
